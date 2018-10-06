32f6f9404944f3725aeeb6f25783512570aa870b
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2018-10-06 17:57:07 -0400

    reset bois

--- events/guildDelete.js
+++
@@ -1,6 +0,0 @@
-// This event executes when a new guild (server) is left.
-
-module.exports = (client, guild) => {
-  // Well they're gone. Let's remove them from the settings!
-  client.settings.delete(guild.id);
-};
