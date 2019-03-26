c660240ebe0c86d5531502759350643cdceaae3f
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2019-03-25 22:10:46 -0400

    added more events

--- cobalt.js
+++ cobalt.js
@@ -32,6 +32,66 @@ cobalt.on('ready', () => {
       }, 3600000);
 });
 
+cobalt.on("messageDelete", (message) => {
+    var channel = cobalt.channels.get('426044465023680513');
+    let author = message.author;
+    let avatar = author.displayAvatarURL;
+
+
+    let deletedMessage = new Discord.RichEmbed()
+        .setTitle('Message deleted')
+        .setAuthor(author.username, avatar)
+        .addField("Channel", message.channel, true)
+        .addField("Content", message.content, true)
+        .setColor("#d62424");
+    
+    channel.send(deletedMessage);
+});
+
+cobalt.on("messageUpdate", (oldMessage, newMessage) => {
+    if (oldMessage.author.bot) return;
+    var channel = cobalt.channels.get('426044465023680513');
+    let author = newMessage.author;
+    let avatar = author.displayAvatarURL;
+
+
+    let updateMessage = new Discord.RichEmbed()
+        .setTitle('Message updated')
+        .setAuthor(author.username, avatar)
+        .addField("Original", oldMessage.cleanContent, true)
+        .addField("Edit", newMessage.cleanContent, true)
+        .setColor("#00a1ff");
+    
+    channel.send(updateMessage);
+});
+
+cobalt.on("guildMemberAdd", (member) => {
+    var channel = cobalt.channels.get('426044465023680513');
+    let memberCount = cobalt.members;
+
+    let newMember = new Discord.RichEmbed()
+        .setTitle('New Cobaltia citizen has joined')
+        .setAuthor(member.username, member.displayAvatarURL)
+        .addField("Member count", memberCount)
+        .setColor("#1cc936");
+
+    channel.send(newMember);
+});
+
+cobalt.on("guildMemberRemove", (member) => {
+    var channel = cobalt.channels.get('426044465023680513');
+    let memberCount = cobalt.members;
+
+    let newMember = new Discord.RichEmbed()
+        .setTitle('Old Cobaltia citizen left or got banned')
+        .setAuthor(member.username, member.displayAvatarURL)
+        .addField("member count", memberCount)
+        .setColor("d62424");
+
+    channel.send(newMember);
+})
+
+
 cobalt.on('message', (message) =>{
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
