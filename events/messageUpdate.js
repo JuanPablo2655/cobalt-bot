a481299c040284abc6ea3b41beecbb8558edc5cd
Juan Pablo <isidroicp@outlook.com>
2019-05-26 21:12:08 -0400

    got rid of some old files and hopefully fixed some "bugs"

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,14 +2,14 @@ const Discord = require("discord.js");
 
 module.exports = (cobalt, oldMessage, newMessage) => {
     if (oldMessage.author.bot) return;
-    var channel = cobalt.channels.get('426044465023680513');
+    var channel = cobalt.channels.get('405158191324987393');
     let author = newMessage.author;
     let avatar = author.displayAvatarURL;
 
     let updateMessage = new Discord.RichEmbed()
         .setTitle('Message updated')
         .setAuthor(author.username, avatar)
-        .addField("Original", `${oldMessage.cleanContent} `, true)
+        .addField("Original", oldMessage.cleanContent + " ", true)
         .addField("Edit", newMessage.cleanContent, true)
         .setColor("#00a1ff");
     channel.send(updateMessage);
