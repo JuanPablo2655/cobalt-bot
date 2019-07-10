0c7455f0689ff2798c4ccddabb14e7bd2a3c05aa
Juan Pablo <isidroicp@outlook.com>
2019-07-10 17:57:41 -0400

    once again recode time

--- model/nations.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const nationSchema = mongoose.Schema({
-    userID: String,
-    nation: String
-});
-
-module.exports = mongoose.model('nation', nationSchema);
